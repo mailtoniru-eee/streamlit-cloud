533f31f8b57d7916878ba061aed8c1dfe0bd1b6d
mailtoniru-eee <34086086+mailtoniru-eee@users.noreply.github.com>
2025-07-02 23:17:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import altair as alt
 from supabase import create_client, Client
 import pandas as pd
 import os
@@ -26,14 +27,13 @@ def get_data():
     response = supabase.table("rag_metrics").select("*").execute()
     return pd.DataFrame(response.data)
 
+df = get_data()
+
 st.subheader("Group 23 - RAG Application - RAGBench Dataset")
 col1, col2, col3 = st.columns([1, 2, 1])  # Center column is wider
 with col2:
     st.image(image, width=300)
 st.markdown("</div>", unsafe_allow_html=True)
-
-
-df = get_data()
 
 tab1, tab2 = st.tabs(["📊 Dashboard", "🖼️ Image Viewer"])
 
